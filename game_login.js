cc20af43568070494a4a59c6636c60ee3fe5fc2b
Ayush-Mohapatra <97773877+Ayush-Mohapatra@users.noreply.github.com>
2022-04-26 09:19:18 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+player1_name=document.getElementById("player1_name_input").value;
+player2_name=document.getElementById("player2_name_input").value;
+localStorage.setItem("Player1 Name",player1_name);
+localStorage.setItem("Player2 Name",player2_name);
+window.location="game_page.html";
+}
